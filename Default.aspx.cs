70eee5d87c5ec336e63ebe97467ed125f0015882
zia <letscode786@gmail.com>
2022-03-19 12:42:54 +0500

    added method to notify external user

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -18,12 +18,63 @@ namespace OneSignalDemo
         {
             if (!IsPostBack)
             {
-                
+
 
                 //System.Diagnostics.Debug.WriteLine(responseContent);
+                NotifyExternal("2");
+                //NotifyPlayer("1", "hello b");
             }
         }
 
+        public void NotifyExternal(string externalId)
+        {
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+
+            request.Headers.Add("authorization", "Basic " + ConfigurationManager.AppSettings["OneSignalApiKey"]);
+
+            var serializer = new JavaScriptSerializer();
+            var obj = new
+            {
+                app_id = ConfigurationManager.AppSettings["OneSignalAppID"],
+                contents = new { en = "English Message" },
+                channel_for_external_user_ids = "push",
+                include_external_user_ids = new string[] { externalId }
+            };
+
+
+
+            var param = serializer.Serialize(obj);
+            byte[] byteArray = Encoding.UTF8.GetBytes(param);
+
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+
+            System.Diagnostics.Debug.WriteLine(responseContent);
+        }
         public void NotifyPlayer(string playerId, string message)
         {
             string appId = ConfigurationManager.AppSettings["OneSignalAppID"];
@@ -62,7 +113,7 @@ namespace OneSignalDemo
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                //throw new Exception(ex.Message);
             }
         }
         public void NotifySubscribers(string message)
@@ -107,7 +158,7 @@ namespace OneSignalDemo
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                //throw new Exception(ex.Message);
             }
         }
     }
